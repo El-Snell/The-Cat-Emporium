28467fde4d37a7a489907c5bcc21c55bd1933804
El-Snell <angsutton03@gmail.com>
2026-03-03 13:28:04 -0500

    test 2

--- photogallery.js
+++ photogallery.js
@@ -1,6 +1,11 @@
-lightGallery(document.getElementById('static-thumbnails'), {
-  animateThumb: false,
-  zoomFromOrigin: false,
-  allowMediaOverlap: true,
-  toggleThumb: true,
+lightGallery(document.getElementById('customize-thumbnails-gallery'), {
+    // Add a custom class to apply style only for the particular gallery
+    addClass: 'lg-custom-thumbnails',
+
+    // Remove the starting animations.
+    // This can be done by overriding CSS as well
+    appendThumbnailsTo: '.lg-outer',
+
+    animateThumb: false,
+    allowMediaOverlap: true,
 });
