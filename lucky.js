e235cbdd082939c14800bbceb92083c2bc7063da
El-Snell <angsutton03@gmail.com>
2026-08-06 17:39:16 -0400

    image automation

---
+++ lucky.js
@@ -0,0 +1,13 @@
+const gallery = document.getElementById("lightgallery");
+
+tags.lucky.forEach(num => {
+    const a = document.createElement("a");
+    a.href = `${num}.JPG`;
+
+    const img = document.createElement("img");
+    img.src = `${num}.JPG`;
+    img.alt = `img${num}`;
+
+    a.appendChild(img);
+    gallery.appendChild(a);
+});
