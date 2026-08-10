b985c25b7ccbf8bb3425a2ae1de83e78c3d5a8d9
El-Snell <angsutton03@gmail.com>
2026-08-10 16:27:44 -0400

    search start

---
+++ search.js
@@ -0,0 +1,10 @@
+const bean = document.getElementById("beancheck");
+const bert = document.getElementById("bertcheck");
+const lucky = document.getElementById("luckycheck");
+const soji = document.getElementById("sojicheck");
+const lightgallery = document.getElementById("lightgallery");
+
+// Reload page
+function reload_page() {
+  location.reload;
+}
